6621005a6910a3c55cbfda0cc37897a7a4b7d2c6
Shayan Ali Akbar <sakbar@purdue.edu>
2019-09-14 23:40:16 -0400

    add more features to get info file

--- get_info_from_user_profile.py
+++ get_info_from_user_profile.py
@@ -8,7 +8,7 @@ other information from that webpage if present.
 import sys, os
 import re
 import urllib.request, urllib.error
-import time
+import time, datetime
 from math import ceil
 
 user_file = sys.argv[1] #where is user file
@@ -22,19 +22,28 @@ if os.path.exists(out_file):
 github_page = "https://github.com/" #homepage for github
 
 counter = start_users #counter for users in the file
-num_users_done = 0 #num of users done
 percent_users_to_write = 5 #this percent users to write inside the loop (kind of like event)
-num_users_to_write = ceil((percent_users_to_write/100) * (end_users - start_users))
+num_users_total = end_users - start_users
+num_users_to_write = ceil((percent_users_to_write/100) * num_users_total)
+
+num_users_done = 0 #num of users done
 num_users_couldnot_find = 0 #couldn't find this many users
+num_users_no_country = 0
+num_users_no_langs = 0
+num_users_no_repos = 0
+num_users_no_descs = 0
+
 users = open(user_file) #users file to read
 for i in range(start_users): users.next()
-start = time.time() #time
+
+start_time = time.time() #time
 mapsd = {} #data to store (a dict for each user and info)
 
 print ("Reading users file from", start_users, "to", end_users)
 print ("Percent users to write at a time", percent_users_to_write)
 print ("Num users to write at a time", num_users_to_write)
 for u in users: #loop over all the users in the file
+	num_users_done += 1
 	u_split = u.strip().split()
 	user = u_split[0]
 	num_commits = u_split[1]
@@ -43,49 +52,63 @@ for u in users: #loop over all the users in the file
 	counter += 1
 	
 	user_homepage = github_page + user + "?tab=repositories"
-	print (user_homepage)
 	try:
 		conn = urllib.request.urlopen(user_homepage)
 	except urllib.error.HTTPError as e:
-		print ("Couln't find ", user_homepage)
 		num_users_couldnot_find += 1
 	except urllib.error.URLError as e:
-		print ("Couln't find ", user_homepage)
 		num_users_couldnot_find += 1
 	else:
 		data = conn.read().decode('utf-8')
 
 		match_country = re.search('\"Home location: (.*)\" class', data)
 		if match_country: country = match_country.group(1)
-		else: continue
+		else: 	
+			num_users_no_country += 1
+			continue
 
 		match_langs = re.findall('<span itemprop=\"programmingLanguage\">(.*)</span>', data)
 		if match_langs:	langs = match_langs
-		else: langs = []
+		else: 
+			num_users_no_langs += 1
+			langs = []
 
 		match_reponames = re.findall('itemprop=\"name codeRepository\" >\n(.*)</a>', data)
 		if match_reponames: reponames = [x.strip() for x in match_reponames]
-		else: reponames = []
+		else: 
+			num_users_no_repos += 1
+			reponames = []
 
 		match_repodescs = re.findall('itemprop=\"description\">\n(.*)',data)
-		print ("\n")
-		if match_repodescs: 
-			for x in match_repodescs: print (x)
-		else: repodescs = []
+		if match_repodescs: repodescs = match_repodescs
+		else: 
+			num_users_no_descs += 1
+			repodescs = []
 
-		mapsd[user] = [num_commits, country, langs]
+		mapsd[user] = [num_users_done, num_commits, country, langs, reponames, repodescs]
 		#print (mapsd[user])
-	
-	num_users_done += 1
-	if num_users_done % num_users_to_write == 0:  
+
+	if num_users_done % num_users_to_write == 0 or num_users_done == num_users_total:  
 		with open(out_file, "a") as map_f:
 				for u_map, u_val in mapsd.items():
-					map_f.write(u_map +
-\
-" ;;; " + u_val[0] + " ;;; " + u_val[1] + " ;;; " +
-",".join(u_val[2]) + " ;;; " + ",".join(u_val[3] + ";;".join(u_val[4]) + "\n")
+					map_f.write(str(u_val[0]) + " ;;; " + 
+							u_map + " ;;; " + 
+							u_val[1] + " ;;; " + 
+							u_val[2] + " ;;; " + 
+							",".join(u_val[3]) + " ;;; " + 
+							",".join(u_val[4]) + " ;;; " +
+							 ";;".join(u_val[5]) + "\n")
+
+		print ("Users:: Total:", num_users_total ,", Done: ", num_users_done, 
+			", 404 Not found: ", num_users_couldnot_find, 
+			", No country: ", num_users_no_country, 
+			", No languages: ", num_users_no_langs, 
+			", No repositories: ", num_users_no_repos, 
+			", No descriptions: ", num_users_no_descs, 
+			", Time:: ", datetime.timedelta(seconds=time.time() - start_time))
+
 		mapsd = {}
 				
 
 print ("Data saved at ", out_file)
-print ("Done in time:" , time.time()-start)
+print ("Done in time:" , time.time()-start_time)
